9cef20769c864860465d4c58a3ce99dea8a0e930
Leandro Antunes <lele.tune@gmail.com>
2021-10-29 19:14:57 -0300

    Fontes do Google Fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,7 +14,7 @@ export const GlobalStyle = createGlobalStyle`
         --background: #F0F2F5;
         --shape: #FFFFFF;
     }
-
+   
     * {
         margin: 0;
         padding: 0;
@@ -23,17 +23,26 @@ export const GlobalStyle = createGlobalStyle`
 
     html {
         @media (max-width: 1080px) {
-            font-size:93,75%;
+            font-size:93,75%; // 15px
         }
 
         @media (max-width: 720px) {
-            font-size:87,5%;
+            font-size:87,5%; // 14px
         }
     }
 
     body {
         background: var(--background);
         --webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
     }
 
     button {
