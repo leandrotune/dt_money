d863828dd8e9df51916906cce6fa93e6eb3130b2
Leandro Antunes <lele.tune@gmail.com>
2021-10-31 20:42:26 -0300

    Componente: Summary

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
- import  logoImg  from "../../assets/logo.svg"
+import  logoImg  from "../../assets/logo.svg"
 import { Container, Content } from "./styles"
 
  export function Header() {
     return (
         <Container>
             <Content>
-                <img src={ logoImg } alt="" />
+                <img src={ logoImg } alt="dt money" />
                 <button>
                     Nova transação
                 </button>
