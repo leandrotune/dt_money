e57f5424264b4ddb82162725d4f20f2219991d2e
Leandro Antunes <lele.tune@gmail.com>
2021-11-01 22:33:27 -0300

    Componente: Transactions Table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { Transactionstable } from "../TransactionsTable";
 import { Container } from "./styled";
 
 export function Dashboard() {
     return(
         <Container>
             <Summary />
+            <Transactionstable />
         </Container>
     )
 }
